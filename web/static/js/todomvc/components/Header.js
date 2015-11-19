ea810d7d2827b2f4d6c0ed9477ba260926fcb414
Kevin Old <kevin@kevinold.com>
2015-11-18 18:25:00 -0600

    add todomvc styling

--- web/static/js/todomvc/components/Header.js
+++ web/static/js/todomvc/components/Header.js
@@ -15,7 +15,7 @@ export default class Header extends Component {
   render() {
     return (
       <header className='header'>
-          <h1>todos!!!!</h1>
+          <h1>Todos</h1>
           <TodoTextInput newTodo={true}
                          onSave={::this.handleSave}
                          placeholder='What needs to be done?' />
